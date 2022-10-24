c6450112e0c8be14cf2f4a51b6624f80de23bcf8
Darkborderman <reastw1234@gmail.com>
2022-10-24 20:31:35 +0800

    refactor: Update

---
+++ Source/MoreGenes/GeneDef_BodySize.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+using HarmonyLib;
+
+[HarmonyPatch(typeof(Pawn), "BodySize", MethodType.Getter)]
+public static class Pawn_BodySize
+{
+    public static void Postfix(ref float __result, Pawn __instance)
+    {
+        if(__instance != null && __instance.genes != null)
+        {
+            if (__instance.genes.HasGene(DefDatabase<GeneDef>.GetNamed("LargeBodySize")))
+            {
+                __result = (float)(__result * 1.25);
+            }
+            if(__instance.genes.HasGene(DefDatabase<GeneDef>.GetNamed("SmallBodySize")))
+            {
+                __result = (float)(__result * 0.75);
+            }
+        }
+    }
+}
